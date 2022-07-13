1a234f1248ed714c08f1df89611120e65e373bb4
JasonMarshall <Jason.Marshall@insight.com>
2022-07-13 18:30:59 -0400

    Testing

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>3.0.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tf-init-example-rg"
+    storage_account_name = "tformstoracct"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Your code goes here
+
+# An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
