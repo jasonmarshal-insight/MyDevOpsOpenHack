4304357ce85b41579f5508435fc83a1e95b355b8
JasonMarshall <Jason.Marshall@insight.com>
2022-07-13 18:11:10 -0400

    testing this out

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -1,18 +1,18 @@
 terraform {
-    backend "remote" {
-        # The name of your Terraform Cloud organization.
-        organization = "example-organization"
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "example-organization"
 
-        # The name of the Terraform Cloud workspace to store Terraform state files in.
-        workspaces {
-            name = "example-workspace"
-        }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "example-workspace"
     }
+  }
 }
 
 # An example resource that does nothing.
 resource "null_resource" "example" {
-    triggers = {
-        value = "A example resource that does nothing!"
-    }
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
 }
